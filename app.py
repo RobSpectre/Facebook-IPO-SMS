60b5b0b7e0f66b7b27e55cdd9c2c7dc15c4f61c1
Rob Spectre <rob.spectre@gmail.com>
2012-05-18 11:43:07 -0400

    First draft.

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 import os
 
 from flask import Flask
-from flask import render_template
-from flask import url_for
 from flask import request
+import requests
+import simplejson as json
 
 from twilio import twiml
-from twilio.util import TwilioCapability
-
-import local_settings
 
 
 # Declare and configure application
@@ -20,8 +17,8 @@ app.config.from_pyfile('local_settings.py')
 @app.route('/voice', methods=['GET', 'POST'])
 def voice():
     response = twiml.Response()
-    response.say("Congratulations! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    quote = getFBQuote()
+    response.say(quote)
     return str(response)
 
 
@@ -29,39 +26,32 @@ def voice():
 @app.route('/sms', methods=['GET', 'POST'])
 def sms():
     response = twiml.Response()
-    response.sms("Congratulation! You deployed the Twilio Hackpack" \
-            " for Heroku and Flask.")
+    quote = getFBQuote()
+    response.sms(quote)
     return str(response)
 
 
-# Twilio Client demo template
-@app.route('/client')
-def client():
-    configuration_error = None
-    for key in ['TWILIO_ACCOUNT_SID', 'TWILIO_AUTH_TOKEN', 'TWILIO_APP_SID',
-            'TWILIO_CALLER_ID']:
-        if not app.config[key]:
-            configuration_error = "Missing from local_settings.py: " \
-                    "%s" % key
-            token = None
-    if not configuration_error:
-        capability = TwilioCapability(app.config['TWILIO_ACCOUNT_SID'],
-            app.config['TWILIO_AUTH_TOKEN'])
-        capability.allow_client_incoming("joey_ramone")
-        capability.allow_client_outgoing(app.config['TWILIO_APP_SID'])
-        token = capability.generate()
-    return render_template('client.html', token=token,
-            configuration_error=configuration_error)
+def getFBQuote():
+    response = requests.get(
+            "http://dev.markitondemand.com/Api/Quote/json?symbol=FB")
+    try:
+        if response.status == 200:
+            data = json.loads(response.text)
+    except:
+        return "Could not retrieve stock data.  Try again shortly."
+    return "Last Price: %s Change %: %s  Timestamp: %s" % \
+        (data['Data']['LastPrice'], data['Data']['ChangePercent'],
+                data['Data']['Timestamp'])
 
 
-# Installation success page
-@app.route('/')
-def index():
-    params = {
-        'voice_request_url': url_for('.voice', _external=True),
-        'sms_request_url': url_for('.sms', _external=True),
-        'client_url': url_for('.client', _external=True)}
-    return render_template('index.html', params=params)
+@app.errorhandler(500)
+def server_error():
+    response = twiml.Response()
+    if request.form['CallSid']:
+        response.say("FB IPO is currently unavailable.  Try again shortly.")
+    else:
+        response.sms("FB IPO is currently unavailable.  Try again shortly.")
+    return str(response)
 
 
 # If PORT not specified by environment, assume development config.
--- tests/test_twilio.py
+++ tests/test_twilio.py
@@ -1,5 +1,10 @@
 import unittest
+from mock import patch
+from mock import Mock
+import requests
+
 from .context import app
+
 
 class TwiMLTest(unittest.TestCase):
     def setUp(self):
@@ -38,10 +43,58 @@ class TwiMLTest(unittest.TestCase):
 
 
 class ExampleTests(TwiMLTest):
-    def test_sms(self):
+    @patch.object(requests, 'get')
+    def test_sms(self, mock_get):
+        test_file = file('./tests/test_assets/good_quote.json')
+        mock_response = Mock()
+        mock_response.text = test_file.read()
+        mock_response.status = 200
+        mock_get.return_value = mock_response
         response = self.sms("Test")
         self.assertTwiML(response)
+        self.assertTrue("<Sms" in response.data, "Did not receive Sms " \
+                "verb, instead: %s" % response.data)
+        self.assertTrue("Last Price" in response.data, "Did not receive " \
+                "stock quote, instead: %s" % response.data)
 
-    def test_voice(self):
+    @patch.object(requests, 'get')
+    def test_smsBad(self, mock_get):
+        test_file = file('./tests/test_assets/bad_quote.json')
+        mock_response = Mock()
+        mock_response.text = test_file.read()
+        mock_response.status = 200
+        mock_get.return_value = mock_response
+        response = self.sms("Test")
+        self.assertTwiML(response)
+        self.assertTrue("<Sms" in response.data, "Did not receive Sms " \
+                "verb, instead: %s" % response.data)
+        self.assertTrue("Try again shortly" in response.data, "Did not " \
+                "receive error message, instead: %s" % response.data)
+
+    @patch.object(requests, 'get')
+    def test_call(self, mock_get):
+        test_file = file('./tests/test_assets/good_quote.json')
+        mock_response = Mock()
+        mock_response.text = test_file.read()
+        mock_response.status = 200
+        mock_get.return_value = mock_response
         response = self.call()
         self.assertTwiML(response)
+        self.assertTrue("<Say" in response.data, "Did not receive Say " \
+                "verb, instead: %s" % response.data)
+        self.assertTrue("Last Price" in response.data, "Did not receive " \
+                "stock quote, instead: %s" % response.data)
+
+    @patch.object(requests, 'get')
+    def test_callBad(self, mock_get):
+        test_file = file('./tests/test_assets/bad_quote.json')
+        mock_response = Mock()
+        mock_response.text = test_file.read()
+        mock_response.status = 200
+        mock_get.return_value = mock_response
+        response = self.call()
+        self.assertTwiML(response)
+        self.assertTrue("<Say" in response.data, "Did not receive Say " \
+                "verb, instead: %s" % response.data)
+        self.assertTrue("Try again shortly" in response.data, "Did not " \
+                "receive error message, instead: %s" % response.data)
